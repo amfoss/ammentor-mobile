79ce7aa56ec5f06b3013e46e581d27325ff8eef0
ganidande905 <ganidande9@gmail.com>
2025-06-20 01:08:10 +0530

    change submission screen

--- lib/screen/review/view/submission_screen.dart
+++ lib/screen/review/view/submission_screen.dart
@@ -1,11 +1,13 @@
-import 'package:intl/intl.dart';
+import 'package:ammentor/components/custom_calender.dart';
 import 'package:flutter/material.dart';
-import 'package:hugeicons/hugeicons.dart';
-import 'package:ammentor/components/theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:intl/intl.dart';
+import 'package:ammentor/components/theme.dart';
 import 'package:ammentor/components/submission_input_row.dart';
 import 'package:ammentor/screen/review/model/review_model.dart';
 import 'package:ammentor/screen/review/provider/submission_controller.dart';
+
 
 class TaskSubmissionScreen extends ConsumerStatefulWidget {
   final ReviewTask task;
@@ -13,52 +15,17 @@ class TaskSubmissionScreen extends ConsumerStatefulWidget {
   const TaskSubmissionScreen({super.key, required this.task});
 
   @override
-  ConsumerState<TaskSubmissionScreen> createState() =>
-      _TaskSubmissionScreenState();
+  ConsumerState<TaskSubmissionScreen> createState() => _TaskSubmissionScreenState();
 }
 
 class _TaskSubmissionScreenState extends ConsumerState<TaskSubmissionScreen> {
   final TextEditingController _startDateController = TextEditingController();
-  final TextEditingController _submissionLinkController =
-      TextEditingController();
-
-  Future<void> _selectDate(
-    BuildContext context,
-    TextEditingController controller,
-  ) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2030),
-      builder: (BuildContext context, Widget? child) {
-        return Theme(
-          data: Theme.of(context).copyWith(
-            colorScheme: ColorScheme.dark(
-              primary: AppColors.primary,
-              secondary: AppColors.grey,
-            ),
-            textButtonTheme: TextButtonThemeData(
-              style: TextButton.styleFrom(foregroundColor: AppColors.primary),
-            ),
-          ),
-          child: child!,
-        );
-      },
-    );
-    if (picked != null) {
-      setState(() {
-        controller.text = DateFormat('yyyy-MM-dd').format(picked);
-      });
-    }
-  }
+  final TextEditingController _submissionLinkController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    final submissionState =
-        ref.watch(taskSubmissionControllerProvider(widget.task.taskNumber));
-    final controller =
-        ref.read(taskSubmissionControllerProvider(widget.task.taskNumber).notifier);
+    final submissionState = ref.watch(taskSubmissionControllerProvider(widget.task.taskNumber));
+    final controller = ref.read(taskSubmissionControllerProvider(widget.task.taskNumber).notifier);
 
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
@@ -67,11 +34,7 @@ class _TaskSubmissionScreenState extends ConsumerState<TaskSubmissionScreen> {
       resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.background,
       appBar: AppBar(
-      
-        title: Text(
-          'Submission',
-          style: AppTextStyles.subheading(context).copyWith(color: AppColors.white),
-        ),
+        title: Text('Submission', style: AppTextStyles.subheading(context).copyWith(color: AppColors.white)),
         backgroundColor: AppColors.background,
         elevation: 0,
         iconTheme: const IconThemeData(color: AppColors.white),
@@ -87,23 +50,30 @@ class _TaskSubmissionScreenState extends ConsumerState<TaskSubmissionScreen> {
                 Expanded(
                   child: Text(
                     widget.task.taskName,
-                    style: AppTextStyles.body(context).copyWith(
-                      fontWeight: FontWeight.w500,
+                    style: AppTextStyles.subheading(context).copyWith(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ],
             ),
-            SizedBox(
-              height: screenHeight * 0.03,
-              child: Divider(color: AppColors.grey, thickness: 1.0),
-            ),
+            SizedBox(height: screenHeight * 0.03, child: Divider(color: AppColors.grey, thickness: 1.0)),
             SubmissionInputRow(
               icon: HugeIcons.strokeRoundedSunrise,
               text: 'Starting date',
               controller: _startDateController,
               readOnly: true,
-              onTap: () => _selectDate(context, _startDateController),
+              onTap: () async {
+                await showModernCalendarPicker(
+                  context: context,
+                  selectedDate: DateTime.now(),
+                  onDateSelected: (pickedDate) {
+                    _startDateController.text = DateFormat('yyyy-MM-dd').format(pickedDate);
+                  },
+                );
+              },
             ),
             SizedBox(height: screenHeight * 0.012),
             SubmissionInputRow(
@@ -121,8 +91,7 @@ class _TaskSubmissionScreenState extends ConsumerState<TaskSubmissionScreen> {
                       ? null
                       : () async {
                           try {
-                            final DateTime parsedStartDate =
-                                DateTime.parse(_startDateController.text);
+                            final DateTime parsedStartDate = DateTime.parse(_startDateController.text);
                             final int trackId = widget.task.trackId;
 
                             await controller.submitTask(
@@ -132,43 +101,28 @@ class _TaskSubmissionScreenState extends ConsumerState<TaskSubmissionScreen> {
                               parsedStartDate,
                             );
 
-                            if (ref
-                                .read(taskSubmissionControllerProvider(
-                                        widget.task.taskNumber))
-                                .isSubmissionSuccessful) {
+                            if (ref.read(taskSubmissionControllerProvider(widget.task.taskNumber)).isSubmissionSuccessful) {
                               Navigator.of(context).pop();
                             } else {
                               ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text(
-                                      'Submission failed. Please try again.'),
-                                ),
+                                const SnackBar(content: Text('Submission failed. Please try again.')),
                               );
                             }
                           } catch (e) {
                             ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content:
-                                    Text('Invalid input: ${e.toString()}'),
-                              ),
+                              SnackBar(content: Text('Invalid input: ${e.toString()}')),
                             );
                           }
                         },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.amber,
                     foregroundColor: Colors.black,
-                    padding:
-                        EdgeInsets.symmetric(vertical: screenHeight * 0.016),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+                    padding: EdgeInsets.symmetric(vertical: screenHeight * 0.016),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14.0)),
                   ),
                   child: submissionState.isSubmitting
                       ? const CircularProgressIndicator()
-                      : Text(
-                          'Submit for review',
-                          style: AppTextStyles.button(context),
-                        ),
+                      : Text('Submit for review', style: AppTextStyles.button(context)),
                 ),
               ),
             ),
