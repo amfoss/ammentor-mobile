bd2c28fbdd717b2657bd94f96a2983952995ef46
ganidande905 <ganidande9@gmail.com>
2025-06-20 00:53:58 +0530

    add new submit task tile

--- lib/screen/mentees/mentee_dashboard.dart
+++ lib/screen/mentees/mentee_dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:ammentor/components/theme.dart';
 import 'package:ammentor/screen/leaderboard/view/leaderboard_screen.dart';
 import 'package:ammentor/screen/profile/view/profile_screen.dart';
+import 'package:ammentor/screen/review/view/review_screen.dart';
 import 'package:ammentor/screen/track/view/track_screen.dart';
 import 'package:ammentor/screen/evaluation/view/mentee_list_screen.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,7 @@ class _MenteeHomePageState extends State<MenteeHomePage> {
   final List<Widget> _pages = [
     LeaderboardScreen(),
     TracksScreen(),
-    MenteeListScreen(),
+    TaskReviewScreen(),
     ProfileScreen(),
   ];
 
